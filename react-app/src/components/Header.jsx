989d528b6921586fdc469ae66b039376057f6e7e
Michael Aubry <michealaubrydesign@gmail.com>
2023-09-19 19:28:01 +0200

    Ajout des <Link>

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import logo from '../assets/argentBankLogo.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/argentBankLogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
     return (
         <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
-          <img src={logo} alt="Argent Bank Logo" className="main-nav-logo-image" />
-          <h1 className="sr-only">Argent Bank</h1>
-        </a>
-        <div>
-          <a className="main-nav-item" href="./sign-in.html">
-          <FontAwesomeIcon icon={faUserCircle} />
-            Sign In
-          </a>
-        </div>
-      </nav>
-    )
+            <Link to="/" className="main-nav-logo">
+                <img
+                    src={logo}
+                    alt="Argent Bank Logo"
+                    className="main-nav-logo-image"
+                />
+                <h1 className="sr-only">Argent Bank</h1>
+            </Link>
+            <div>
+                <Link to="/signin" className="main-nav-item">
+                    <FontAwesomeIcon icon={faUserCircle} />
+                    Sign In
+                </Link>
+            </div>
+        </nav>
+    );
 }
 
-export default Header
+export default Header;
