22360869e7376af3b425e7be8df3fb59c3fbd516
Michael Aubry <michealaubrydesign@gmail.com>
2023-10-06 12:35:25 +0200

    Déconnexion utilisateur et affichage username

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import logo from '../assets/argentBankLogo.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user);
+    const isAuthenticated = user && user.token;
+    const userName = user && user.userName;
+
+    const handleSignOut = () => {
+        dispatch({
+            type: 'SET_USER',
+            payload: {
+                userName: null,
+                firstName: null,
+                lastName: null,
+                token: null,
+            },
+        });
+    };
+
     return (
         <nav className="main-nav">
             <Link to="/" className="main-nav-logo">
@@ -16,10 +34,25 @@ function Header() {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link to="/signin" className="main-nav-item">
-                    <FontAwesomeIcon icon={faUserCircle} />
-                    Sign In
-                </Link>
+                {isAuthenticated ? (
+                    <div className="main-nav-user">
+                        <span className="username">{userName}</span>
+                        <Link
+                            to="/"
+                            className="main-nav-item"
+                            onClick={handleSignOut}
+                        >
+                            <FontAwesomeIcon icon={faUserCircle} />
+
+                            <span onClick={handleSignOut}>Sign Out</span>
+                        </Link>
+                    </div>
+                ) : (
+                    <Link to="/signin" className="main-nav-item">
+                        <FontAwesomeIcon icon={faUserCircle} />
+                        Sign In
+                    </Link>
+                )}
             </div>
         </nav>
     );
