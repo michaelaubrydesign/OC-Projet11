849955b4aa0e0a78c792ffd923bb76d064186a5c
Michael Aubry <michealaubrydesign@gmail.com>
2023-10-06 12:33:34 +0200

    Mise à jour du store

--- react-app/src/store.js
+++ react-app/src/store.js
@@ -2,7 +2,6 @@ import { combineReducers, createStore } from 'redux';
 
 // Import des réducteurs individuels ici
 import userReducer from './reducers/userReducer';
-import authReducer from './reducers/authReducer';
 
 const reduxDevtools =
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
@@ -11,7 +10,6 @@ const reduxDevtools =
 // Utilisez combineReducers pour combiner tous les réducteurs en un seul
 const reducer = combineReducers({
     user: userReducer,
-    auth: authReducer,
 });
 
 const store = createStore(reducer, reduxDevtools);
